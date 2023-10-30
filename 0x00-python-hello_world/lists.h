7edbf1e92f38ef5d85fd513f270d5eb15ef3547c
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-30 22:50:21 +0200

    testing cases

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -8,7 +8,8 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node 
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -16,6 +17,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif
