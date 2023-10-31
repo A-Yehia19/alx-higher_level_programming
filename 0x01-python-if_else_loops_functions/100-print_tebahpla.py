5d2b6f250665b04c3268aabbb5caf73072f725e6
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-31 21:42:31 +0200

    TEST CASE

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a') - 1, -1):
-    char = i
-    if i % 2 != 0:
-        char = char - 32
-    print("{:c}".format(char), end='')
-    
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
