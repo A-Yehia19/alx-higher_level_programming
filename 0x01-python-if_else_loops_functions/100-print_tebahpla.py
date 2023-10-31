e77214a1cd977b619f12f2e73f2ebeabde8e6355
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-10-31 21:45:16 +0200

    added permissions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-for c in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(c, chr(c - 33)), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    char = i
+    if i % 2 != 0:
+        char = char - 32
+    print("{:c}".format(char), end='')
