19eb1d128864b41545be6257de8bdf718586a7b7
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-02 16:59:22 +0200

    init run

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+import sys
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
+        print('0 arguments.')
+    elif argc == 1:
+        print('1 argument:')
+    else:
+        print('{:d} arguments:'.format(argc))
+
+    for index, arg in enumerate(argv, 1):
+        print('{:d}: {:s}'.format(index, arg))
