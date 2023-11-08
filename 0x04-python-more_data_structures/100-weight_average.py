3bdd0ec80d564a02375f938c7c5f1c56cf601f27
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-08 23:52:57 +0200

    adv run

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    pass
+    if len(my_list) == 0:
+        return 0
+
+    ans = 0
+    quotient = 0
+    for item in my_list:
+        ans += item[0] * item[1]
+        quotient += item[1]
+
+    return ans / quotient
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    pass
+    if value is not None or value not in a_dictionary.values():
+        for key, val in list(a_dictionary.items()):
+            if val == value:
+                del a_dictionary[key]
+
+    return a_dictionary
