9311e04c38d413a2038da3f360b6c7c7d405944e
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-21 12:15:56 +0200

    init run

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+''' Square Class'''
+
+
 class Square:
-    pass
+    ''' Define a Square.
+        Attributes:
+            size (int): Size of square. -> private instance attribute
+    '''
+    def __init__(self, size):
+        ''' Initialize Square.
+            Args:
+                size (int): Size of square.
+        '''
+        self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 
+''' Square Class'''
+
+
 class Square:
-    pass
+    ''' Define a Square.
+        Attributes:
+            size (int): Size of square. -> private instance attribute
+    '''
+    def __init__(self, size=0):
+        ''' Initialize Square.
+            Args:
+                size (int): Size of square.
+        '''
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
