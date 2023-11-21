c7cddb3517a02e401f9b9ff87deb0d9944c15f27
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-11-21 11:44:38 +0200

    init run

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    pass
+    ans = None
+    try:
+        ans = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(ans))
+    return ans
