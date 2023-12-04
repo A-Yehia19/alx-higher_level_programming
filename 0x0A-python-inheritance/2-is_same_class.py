0999f40930d21e151e11c21d6aa8f2cd8f29b107
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-04 12:06:28 +0200

    testing case

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""This module contains a function is_same_class"""
+
 
 def is_same_class(obj, a_class):
     """Function that returns True if the object is exactly an instance of the
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""This module contains a function inherits_from"""
+
+
 def inherits_from(obj, a_class):
     """Function that returns True if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class ; otherwise
