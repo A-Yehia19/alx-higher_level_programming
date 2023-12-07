181a69273e080daa47f9386b71a74b9707844f53
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-07 13:20:17 +0200

    init run

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 
+'''Pascal's Triangle module'''
+
+
 def pascal_triangle(n):
-    pass
+    '''Returns a list of lists of integers representing the Pascal's triangle
+    of n'''
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            # The value at triangle[i][j] is the sum of the values
+            # immediately above and to the left and above and to the right.
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
