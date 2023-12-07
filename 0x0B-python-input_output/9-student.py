d5619149eeaff3f45f4def099569892621e6f728
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-07 12:59:39 +0200

    init run

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,4 +1,49 @@
 #!/usr/bin/python3
 
+'''Student class module'''
+
+
 class Student:
-    pass
+    '''Student class
+
+    Attributes:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+
+    Raises:
+        TypeError: if first_name is not a string
+        TypeError: if last_name is not a string
+        TypeError: if age is not an integer
+        ValueError: if age is less than 0
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initializes an instance
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int): age of student
+
+        Raises:
+            TypeError: if first_name is not a string
+            TypeError: if last_name is not a string
+            TypeError: if age is not an integer
+            ValueError: if age is less than 0
+        '''
+        if type(first_name) is not str:
+            raise TypeError('first_name must be a string')
+        if type(last_name) is not str:
+            raise TypeError('last_name must be a string')
+        if type(age) is not int:
+            raise TypeError('age must be an integer')
+        if age < 0:
+            raise ValueError('age must be >= 0')
+
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+
+    def to_json(self):
+        '''Returns the dictionary representation of a Student instance'''
+        return self.__dict__
