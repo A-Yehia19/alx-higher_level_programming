6795ed84631a129460fbb1f4c929479a1a2c07a6
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-07 12:46:21 +0200

    FIXING ISSUE

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,7 +8,12 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 def add_item(args):
     '''Adds all arguments to a Python list, and then save them to a file'''
-    old_list = load_from_json_file('add_item.json')
+
+    try:
+        old_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        old_list = []
+
     new_list = old_list + args
     save_to_json_file(new_list, 'add_item.json')
 
