05e9d683d22ef16ee1cc0037e87453f022141153
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-12-07 12:38:28 +0200

    testig cases

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,5 @@ def save_to_json_file(my_obj, filename):
     '''Writes an Object to a text file, using a JSON representation'''
     import json
 
-    json_text = json.dumps(my_obj)
     with open(file=filename, mode='w', encoding='utf-8') as f:
-        f.write(json_text)
+        json.dump(obj=my_obj, fp=f)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv[1:]
-old_list = load_from_json_file('add_item.json')
-new_list = old_list + args
-save_to_json_file(new_list, 'add_item.json')
+def add_item(args):
+    '''Adds all arguments to a Python list, and then save them to a file'''
+    old_list = load_from_json_file('add_item.json')
+    new_list = old_list + args
+    save_to_json_file(new_list, 'add_item.json')
+
+
+if __name__ == '__main__':
+    add_item(sys.argv[1:])
