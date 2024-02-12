934c2251128e7032a5f59e86b39f3be3e2b58b02
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-02-12 20:10:36 +0200

    test

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const len = arr.length;
 let i = 0;
 
-for (i = 0; i < arr.length; i++) {
+for (i = 0; i < len; i++) {
   console.log(arr[i]);
 }
