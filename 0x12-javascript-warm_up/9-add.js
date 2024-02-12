a329dfda054c590554b140ed9e94b9a7dc392860
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-02-12 20:07:02 +0200

    fixed typo

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ function add (a, b) {
 }
 
 const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(arprocess.argvgv[3]);
+const num2 = parseInt(process.argv[3]);
 
 console.log(add(num1, num2));
