e63a6d6d1befcf8fb0b30e0b793f5ae816a756eb
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-02-13 18:34:04 +0200

    [added]: init run

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+    charPrint (c) {
+        if (c === 'undefined') {
+            c = 'X';
+        }
+        for (let i = 0; i < this.width; i++) {
+            x += c;
+        }
+    
+        for (let j = 0; j < this.height; j++) {
+            console.log(x);
+        }
+    }
+}
