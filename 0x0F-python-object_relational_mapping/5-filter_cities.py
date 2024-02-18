aa4d7b2957075e900bff8d490255d79590772f13
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-02-18 23:55:24 +0200

    code style fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == "__main__":
     """ Main function """
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                            user=argv[1], passwd=argv[2], db=argv[3])
+                           user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     query = "SELECT cities.name FROM cities \
             JOIN states ON cities.state_id = states.id\
